3bc57d848583e30bc5fabbb2b48098b000f6cff3
Cenk Alti <cenkalti@gmail.com>
2013-09-28 12:11:58 +0300

    fix fmt

--- examples/example.go
+++ examples/example.go
@@ -14,9 +14,7 @@ func main() {
 	for i := 0; i < 10; i += 1 {
 		n := r.Intn(5)
 		d := time.Duration(n)*time.Second
-		s.Enter(d, func() {
-				fmt.Println("Call", n)
-			})
+		s.Enter(d, func() {fmt.Println("Call", n)})
 	}
 
 	// Events will be called in order of their specified delay
